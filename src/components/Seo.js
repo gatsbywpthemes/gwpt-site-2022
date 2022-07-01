8aafda8f98c6a041015418475500fceabe3f7655
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2022-07-01 19:36:57 +0530

    Added full url for Graph image and few extra seo tags added

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -6,16 +6,31 @@ const SEO = ({
   description = "Premium Gatsby Themes for WordPress",
   shareImage,
 }) => {
+  let fullImagePath = `https://gatsbywpthemes.com${shareImage}`
   return (
     <Helmet title={title}>
       <meta name="description" content={description} />
-      <meta name="twitter:title" content={title} />
+      {/* <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={shareImage} />
       <meta name="twitter:image" content={shareImage} />
+      <meta name="twitter:card" content="summary_large_image" /> */}
+
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content="Gatsby WP Themes" />
+      <meta property="og:image" content={fullImagePath} />
+      {/* Twitter tags */}
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:creator" content="@alexadark" />
+      <meta name="twitter:site" content="@Gatsby_WP" />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content={fullImagePath} />
+      <meta name="twitter:image:alt" content={shareImage?.altText ?? title} />
     </Helmet>
   )
 }
